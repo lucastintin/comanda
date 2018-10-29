d46797c07e4c402ea726910c2f339d8bb206d9d7
Lucas Amiune <lamiune@gruporcp.local>
2018-10-29 15:36:40 -0300

    Adicionando frontend de cliente

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const bodyParser    = require('body-parser');
 const config  = require('./config/config');
 
 var app = express();
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 
 //Modelos do Sistema
 var {Pedido}    = require('./models/pedido.model');
@@ -15,7 +15,7 @@ var {Cliente}   = require('./models/cliente.model');
 var {Utils}     = require('./utils/utils');
 
 app.get('/', (req, res) => {
-    res.send('Ola Mundo');
+    res.render('index');
 });
 
 app.get('/teste', (req, res) => {
@@ -24,7 +24,7 @@ app.get('/teste', (req, res) => {
 
 //Rotas Cliente
 app.get('/cliente', (req, res) => {
-    res.render();
+    res.render('./cliente/index.ejs');
 });
 
 app.post('/cliente/add', (req, res) => {
@@ -53,6 +53,9 @@ app.get('/cliente/list', (req, res) => {
 });
 
 //Rotas Pedido
+app.get('/pedido', (req, res) => {
+    res.render('./pedido/index.ejs');
+});
 
 //Sevidor
 app.listen(config.PORT, () => {
